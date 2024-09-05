991525ab04d228f3399757b04e2309b005e09cd8
aishwarya <ashp17star@gmail.com>
2024-09-05 12:23:57 +0530

    MVVM Demo

---
+++ 19_06_24_MVVM_Demo/Screens/Product/View/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  19_06_24_MVVM_Demo
+//
+//  Created by Vishal Jagtap on 05/09/24.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 19_06_24_MVVM_Demo/Screens/Product/ViewModel/ProductViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductViewModel.swift
+//  19_06_24_MVVM_Demo
+//
+//  Created by Vishal Jagtap on 05/09/24.
+//
+
+import Foundation
